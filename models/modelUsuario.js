a82760d6ed787ec392897efc5f85a4edbc095a74
juan pablo gil arango <dinosauriotedy@gmail.com>
2024-12-01 02:23:25 -0500

    quitado el models y añadido el validar al controller

--- models/modelUsuario.js
+++
@@ -1,8 +0,0 @@
-export const usuarios = [
-    { email: "usuario1@terra.com", password: "123456" },
-    { email: "usuario2@terra.com", password: "contraseña123" },
-];
-
-export const validarUsuario = (email, password) => {
-    return usuarios.some(usuario => usuario.email === email && usuario.password === password);
-};
